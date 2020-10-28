ed83f01ea26c4f28855b9de32ec525fad61df1d3
Mustafa <codingbox@outlook.com>
2020-10-28 12:42:04 -0400

    phase 1 design, typed.js added, bulma added, bootstrap removed, fontawesome updated

---
+++ scripts/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    var typed = new Typed('#typedjs', {
+          strings: ["student @ Sheridan College. ^2000",
+            "lifelong learner. ^2000",
+            "software developer. ^2000"],
+          typeSpeed:80,
+          backDelay:80,
+    });
+});
